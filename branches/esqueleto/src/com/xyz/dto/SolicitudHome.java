5a7cdaef4cd41eaefb548df3b4ee0afdd1e5b0c2
richypati@gmail.com <richypati@gmail.com@4ee7b030-5375-8fe9-563a-4aab63be8709>
2014-07-05 01:37:22 +0000

    Creación de archivo de configuración de Hibernate. Creación de archivo de configuración de ingeniería reversa de Hibernate. Creación de DTOs y archivos de mapeo hbm mediante ingeniería reversa.

---
+++ branches/esqueleto/src/com/xyz/dto/SolicitudHome.java
@@ -0,0 +1,123 @@
+package com.xyz.dto;
+
+// Generated 4/07/2014 07:36:05 PM by Hibernate Tools 3.4.0.CR1
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Solicitud.
+ * @see com.xyz.dto.Solicitud
+ * @author Hibernate Tools
+ */
+public class SolicitudHome {
+
+	private static final Log log = LogFactory.getLog(SolicitudHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Solicitud transientInstance) {
+		log.debug("persisting Solicitud instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Solicitud instance) {
+		log.debug("attaching dirty Solicitud instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Solicitud instance) {
+		log.debug("attaching clean Solicitud instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Solicitud persistentInstance) {
+		log.debug("deleting Solicitud instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Solicitud merge(Solicitud detachedInstance) {
+		log.debug("merging Solicitud instance");
+		try {
+			Solicitud result = (Solicitud) sessionFactory.getCurrentSession()
+					.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Solicitud findById(java.lang.String id) {
+		log.debug("getting Solicitud instance with id: " + id);
+		try {
+			Solicitud instance = (Solicitud) sessionFactory.getCurrentSession()
+					.get("com.xyz.dto.Solicitud", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Solicitud instance) {
+		log.debug("finding Solicitud instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession()
+					.createCriteria("com.xyz.dto.Solicitud")
+					.add(Example.create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
