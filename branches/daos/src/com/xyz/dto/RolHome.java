80066635b2f5936a48fb65106e846cda64e62562
richypati@gmail.com <richypati@gmail.com@4ee7b030-5375-8fe9-563a-4aab63be8709>
2014-07-05 17:05:45 +0000

    Creación de DAOs. Se corrigieron algunos Warnings de los DTOs. A los aributos de tipo List se les especificó el tipo de dato que contenía la lista.

---
+++ branches/daos/src/com/xyz/dto/RolHome.java
@@ -0,0 +1,123 @@
+package com.xyz.dto;
+
+// Generated 4/07/2014 07:36:05 PM by Hibernate Tools 3.4.0.CR1
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Rol.
+ * @see com.xyz.dto.Rol
+ * @author Hibernate Tools
+ */
+public class RolHome {
+
+	private static final Log log = LogFactory.getLog(RolHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Rol transientInstance) {
+		log.debug("persisting Rol instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Rol instance) {
+		log.debug("attaching dirty Rol instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Rol instance) {
+		log.debug("attaching clean Rol instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Rol persistentInstance) {
+		log.debug("deleting Rol instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Rol merge(Rol detachedInstance) {
+		log.debug("merging Rol instance");
+		try {
+			Rol result = (Rol) sessionFactory.getCurrentSession().merge(
+					detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Rol findById(java.lang.String id) {
+		log.debug("getting Rol instance with id: " + id);
+		try {
+			Rol instance = (Rol) sessionFactory.getCurrentSession().get(
+					"com.xyz.dto.Rol", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Rol instance) {
+		log.debug("finding Rol instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession()
+					.createCriteria("com.xyz.dto.Rol")
+					.add(Example.create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
