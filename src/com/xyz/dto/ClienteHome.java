1f31dff0207bbf6ccc71b6e747762202ab86d716
richypati <richypati@gmail.com>
2014-07-05 01:37:22 +0000

    Creación de archivo de configuración de Hibernate. Creación de archivo de configuración de ingeniería reversa de Hibernate. Creación de DTOs y archivos de mapeo hbm mediante ingeniería reversa.

---
+++ src/com/xyz/dto/ClienteHome.java
@@ -0,0 +1,123 @@
+package com.xyz.dto;
+
+// Generated 4/07/2014 07:36:05 PM by Hibernate Tools 3.4.0.CR1
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Cliente.
+ * @see com.xyz.dto.Cliente
+ * @author Hibernate Tools
+ */
+public class ClienteHome {
+
+	private static final Log log = LogFactory.getLog(ClienteHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Cliente transientInstance) {
+		log.debug("persisting Cliente instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Cliente instance) {
+		log.debug("attaching dirty Cliente instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Cliente instance) {
+		log.debug("attaching clean Cliente instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Cliente persistentInstance) {
+		log.debug("deleting Cliente instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Cliente merge(Cliente detachedInstance) {
+		log.debug("merging Cliente instance");
+		try {
+			Cliente result = (Cliente) sessionFactory.getCurrentSession()
+					.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Cliente findById(com.xyz.dto.ClienteId id) {
+		log.debug("getting Cliente instance with id: " + id);
+		try {
+			Cliente instance = (Cliente) sessionFactory.getCurrentSession()
+					.get("com.xyz.dto.Cliente", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Cliente instance) {
+		log.debug("finding Cliente instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession()
+					.createCriteria("com.xyz.dto.Cliente")
+					.add(Example.create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
---
+++ src/com/xyz/dto/EncuestaHome.java
@@ -0,0 +1,123 @@
+package com.xyz.dto;
+
+// Generated 4/07/2014 07:36:05 PM by Hibernate Tools 3.4.0.CR1
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Encuesta.
+ * @see com.xyz.dto.Encuesta
+ * @author Hibernate Tools
+ */
+public class EncuestaHome {
+
+	private static final Log log = LogFactory.getLog(EncuestaHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Encuesta transientInstance) {
+		log.debug("persisting Encuesta instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Encuesta instance) {
+		log.debug("attaching dirty Encuesta instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Encuesta instance) {
+		log.debug("attaching clean Encuesta instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Encuesta persistentInstance) {
+		log.debug("deleting Encuesta instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Encuesta merge(Encuesta detachedInstance) {
+		log.debug("merging Encuesta instance");
+		try {
+			Encuesta result = (Encuesta) sessionFactory.getCurrentSession()
+					.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Encuesta findById(java.lang.String id) {
+		log.debug("getting Encuesta instance with id: " + id);
+		try {
+			Encuesta instance = (Encuesta) sessionFactory.getCurrentSession()
+					.get("com.xyz.dto.Encuesta", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Encuesta instance) {
+		log.debug("finding Encuesta instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession()
+					.createCriteria("com.xyz.dto.Encuesta")
+					.add(Example.create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
