726bf0db2bdba7b570b75f9c24966119f89fb0f6
richypati@gmail.com <richypati@gmail.com@4ee7b030-5375-8fe9-563a-4aab63be8709>
2014-07-14 16:54:40 +0000

    Se genera la implementación de los DAOs. En cada DAO se implenta de manera básica el LOG.

---
+++ branches/rtf2/IW_PQRS_UdeA/src/main/co/com/inversionesxyz/dao/impl/DetalleClienteHome.java
@@ -0,0 +1,129 @@
+package co.com.inversionesxyz.dao.impl;
+
+// Generated 14/07/2014 11:30:38 AM by Hibernate Tools 3.4.0.CR1
+
+import java.util.List;
+
+import javax.naming.InitialContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+import co.com.inversionesxyz.dto.DetalleCliente;
+
+/**
+ * Home object for domain model class DetalleCliente.
+ * @see .DetalleCliente
+ * @author Hibernate Tools
+ */
+public class DetalleClienteHome {
+
+	private static final Log log = LogFactory.getLog(DetalleClienteHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(DetalleCliente transientInstance) {
+		log.debug("persisting DetalleCliente instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(DetalleCliente instance) {
+		log.debug("attaching dirty DetalleCliente instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	public void attachClean(DetalleCliente instance) {
+		log.debug("attaching clean DetalleCliente instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(DetalleCliente persistentInstance) {
+		log.debug("deleting DetalleCliente instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public DetalleCliente merge(DetalleCliente detachedInstance) {
+		log.debug("merging DetalleCliente instance");
+		try {
+			DetalleCliente result = (DetalleCliente) sessionFactory
+					.getCurrentSession().merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public DetalleCliente findById(java.lang.String id) {
+		log.debug("getting DetalleCliente instance with id: " + id);
+		try {
+			DetalleCliente instance = (DetalleCliente) sessionFactory
+					.getCurrentSession().get("DetalleCliente", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	public List findByExample(DetalleCliente instance) {
+		log.debug("finding DetalleCliente instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession()
+					.createCriteria("DetalleCliente")
+					.add(Example.create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
