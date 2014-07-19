b535966849308aa3e3f1db53340fda8aa6356c92
jenniferperezb@gmail.com <jenniferperezb@gmail.com@4ee7b030-5375-8fe9-563a-4aab63be8709>
2014-07-19 06:18:13 +0000

    - Refactor a DTO y hbm

--- branches/rtf2VA/IW_PQRS_UdeA/src/main/co/com/inversionesxyz/dto/Solicitud.java
+++ branches/rtf2VA/IW_PQRS_UdeA/src/main/co/com/inversionesxyz/dto/Solicitud.java
@@ -4,12 +4,11 @@ import java.util.Date;
 
 public class Solicitud implements java.io.Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	
 	private String id;
 	private Producto producto;
+	private InformacionAnalista informacionAnalista;
 	private String tipo;
 	private String descripcion;
 	private String estado;
@@ -19,136 +18,78 @@ public class Solicitud implements java.io.Serializable {
 	private Date fechaAtencion;
 	private String tipoDocumento;
 	private String nombreCliente;
-	private Encuesta encuesta;
-
-	public Solicitud() {
-	}
-
-	public Solicitud(String id, Producto producto, String tipo,
-			String descripcion, String estado, Date fechaCreacion,
-			String tipoDocumento, String nombreCliente) {
-		this.id = id;
-		this.producto = producto;
-		this.tipo = tipo;
-		this.descripcion = descripcion;
-		this.estado = estado;
-		this.fechaCreacion = fechaCreacion;
-		this.tipoDocumento = tipoDocumento;
-		this.nombreCliente = nombreCliente;
-	}
-
-	public Solicitud(String id, Producto producto, String tipo,
-			String descripcion, String estado, String complejidad,
-			String prioridad, Date fechaCreacion, Date fechaAtencion,
-			String tipoDocumento, String nombreCliente, Encuesta encuesta) {
-		this.id = id;
-		this.producto = producto;
-		this.tipo = tipo;
-		this.descripcion = descripcion;
-		this.estado = estado;
-		this.complejidad = complejidad;
-		this.prioridad = prioridad;
-		this.fechaCreacion = fechaCreacion;
-		this.fechaAtencion = fechaAtencion;
-		this.tipoDocumento = tipoDocumento;
-		this.nombreCliente = nombreCliente;
-		this.encuesta = encuesta;
-	}
 
 	public String getId() {
 		return this.id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public Producto getProducto() {
 		return this.producto;
 	}
-
 	public void setProducto(Producto producto) {
 		this.producto = producto;
 	}
-
 	public String getTipo() {
 		return this.tipo;
 	}
-
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-
 	public String getDescripcion() {
 		return this.descripcion;
 	}
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-
 	public String getEstado() {
 		return this.estado;
 	}
-
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-
 	public String getComplejidad() {
 		return this.complejidad;
 	}
-
 	public void setComplejidad(String complejidad) {
 		this.complejidad = complejidad;
 	}
-
 	public String getPrioridad() {
 		return this.prioridad;
 	}
-
 	public void setPrioridad(String prioridad) {
 		this.prioridad = prioridad;
 	}
-
 	public Date getFechaCreacion() {
 		return this.fechaCreacion;
 	}
-
 	public void setFechaCreacion(Date fechaCreacion) {
 		this.fechaCreacion = fechaCreacion;
 	}
-
 	public Date getFechaAtencion() {
 		return this.fechaAtencion;
 	}
-
 	public void setFechaAtencion(Date fechaAtencion) {
 		this.fechaAtencion = fechaAtencion;
 	}
-
 	public String getTipoDocumento() {
 		return this.tipoDocumento;
 	}
-
 	public void setTipoDocumento(String tipoDocumento) {
 		this.tipoDocumento = tipoDocumento;
 	}
-
 	public String getNombreCliente() {
 		return this.nombreCliente;
 	}
-
 	public void setNombreCliente(String nombreCliente) {
 		this.nombreCliente = nombreCliente;
 	}
-
-	public Encuesta getEncuesta() {
-		return this.encuesta;
+	public InformacionAnalista getInformacionAnalista() {
+		return informacionAnalista;
 	}
-
-	public void setEncuesta(Encuesta encuesta) {
-		this.encuesta = encuesta;
+	public void setInformacionAnalista(InformacionAnalista informacionAnalista) {
+		this.informacionAnalista = informacionAnalista;
 	}
 
 }
