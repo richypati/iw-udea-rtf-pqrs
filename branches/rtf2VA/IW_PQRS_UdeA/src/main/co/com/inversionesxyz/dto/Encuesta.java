838ae0c84bc16a19b8ce3bd10d8312c66716b68d
richypati@gmail.com <richypati@gmail.com@4ee7b030-5375-8fe9-563a-4aab63be8709>
2014-07-19 05:36:25 +0000

    Reconfiguración de propiedades de DTO según BD

--- branches/rtf2VA/IW_PQRS_UdeA/src/main/co/com/inversionesxyz/dto/Encuesta.java
+++ branches/rtf2VA/IW_PQRS_UdeA/src/main/co/com/inversionesxyz/dto/Encuesta.java
@@ -4,34 +4,63 @@ import java.util.Date;
 
 public class Encuesta implements java.io.Serializable {
 
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-	
-	private String idSolicitud;
+	private String solicitudId;
+	private Solicitud solicitud;
 	private String respuestaPreguntaA;
 	private String respuestaPreguntaB;
 	private Date fechaEnvio;
 
-	public String getIdSolicitud() {
-		return idSolicitud;
+	public Encuesta() {
 	}
-	public void setIdSolicitud(String idSolicitud) {
-		this.idSolicitud = idSolicitud;
+
+	public Encuesta(Solicitud solicitud, String respuestaPreguntaA,
+			String respuestaPreguntaB, Date fechaEnvio) {
+		this.solicitud = solicitud;
+		this.respuestaPreguntaA = respuestaPreguntaA;
+		this.respuestaPreguntaB = respuestaPreguntaB;
+		this.fechaEnvio = fechaEnvio;
 	}
+
+	public String getSolicitudId() {
+		return this.solicitudId;
+	}
+
+	public void setSolicitudId(String solicitudId) {
+		this.solicitudId = solicitudId;
+	}
+
+	public Solicitud getSolicitud() {
+		return this.solicitud;
+	}
+
+	public void setSolicitud(Solicitud solicitud) {
+		this.solicitud = solicitud;
+	}
+
 	public String getRespuestaPreguntaA() {
 		return this.respuestaPreguntaA;
 	}
+
 	public void setRespuestaPreguntaA(String respuestaPreguntaA) {
 		this.respuestaPreguntaA = respuestaPreguntaA;
 	}
+
 	public String getRespuestaPreguntaB() {
 		return this.respuestaPreguntaB;
 	}
+
 	public void setRespuestaPreguntaB(String respuestaPreguntaB) {
 		this.respuestaPreguntaB = respuestaPreguntaB;
 	}
+
 	public Date getFechaEnvio() {
 		return this.fechaEnvio;
 	}
+
 	public void setFechaEnvio(Date fechaEnvio) {
 		this.fechaEnvio = fechaEnvio;
 	}
--- branches/rtf2VA/IW_PQRS_UdeA/src/main/co/com/inversionesxyz/dto/Producto.java
+++ branches/rtf2VA/IW_PQRS_UdeA/src/main/co/com/inversionesxyz/dto/Producto.java
@@ -1,45 +1,89 @@
 package co.com.inversionesxyz.dto;
 
+import java.util.HashSet;
+import java.util.Set;
 
 public class Producto implements java.io.Serializable {
 
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-	
 	private String codigo;
 	private Sucursal sucursal;
 	private String nombre;
 	private String tipo;
 	private float valor;
+	private Set<Solicitud> solicituds = new HashSet<Solicitud>(0);
+
+	public Producto() {
+	}
+
+	public Producto(String codigo, Sucursal sucursal, String nombre,
+			String tipo, float valor) {
+		this.codigo = codigo;
+		this.sucursal = sucursal;
+		this.nombre = nombre;
+		this.tipo = tipo;
+		this.valor = valor;
+	}
+
+	public Producto(String codigo, Sucursal sucursal, String nombre,
+			String tipo, float valor, Set<Solicitud> solicituds) {
+		this.codigo = codigo;
+		this.sucursal = sucursal;
+		this.nombre = nombre;
+		this.tipo = tipo;
+		this.valor = valor;
+		this.solicituds = solicituds;
+	}
 
 	public String getCodigo() {
 		return this.codigo;
 	}
+
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
+
 	public Sucursal getSucursal() {
 		return this.sucursal;
 	}
+
 	public void setSucursal(Sucursal sucursal) {
 		this.sucursal = sucursal;
 	}
+
 	public String getNombre() {
 		return this.nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getTipo() {
 		return this.tipo;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
 	public float getValor() {
 		return this.valor;
 	}
+
 	public void setValor(float valor) {
 		this.valor = valor;
 	}
-	
+
+	public Set<Solicitud> getSolicituds() {
+		return this.solicituds;
+	}
+
+	public void setSolicituds(Set<Solicitud> solicituds) {
+		this.solicituds = solicituds;
+	}
+
 }
