db99dd4073c5d1503afc4b57565641d7374ee364
richypati@gmail.com <richypati@gmail.com@4ee7b030-5375-8fe9-563a-4aab63be8709>
2014-07-21 02:39:06 +0000

    Se configuran las interfaces para las clases de la lógica del negocio

--- branches/rtf2VA/IW_PQRS_UdeA/src/main/co/com/inversionesxyz/bl/IUtilBL.java
+++ branches/rtf2VA/IW_PQRS_UdeA/src/main/co/com/inversionesxyz/bl/IUtilBL.java
@@ -1,15 +1,16 @@
 package co.com.inversionesxyz.bl;
 
 import co.com.inversionesxyz.dto.Solicitud;
+import co.com.inversionesxyz.exception.BasicDBOperationException;
 
 public interface IUtilBL {
 
 	public void notificarSolicitudACliente(String idSolicitud,
-			String nombreCliente, String emailCliente);
+			String nombreCliente, String emailCliente) throws BasicDBOperationException;
 
 	public void notificarSolicitudDelegada(String idSolicitud,
-			String nombreAnalista, String emailAnalista);
+			String nombreAnalista, String emailAnalista) throws BasicDBOperationException;
 
 	public void notificarRespuestaALaSolicitud(Solicitud solicitud,
-			String emailCliente);
+			String emailCliente) throws BasicDBOperationException;
 }
