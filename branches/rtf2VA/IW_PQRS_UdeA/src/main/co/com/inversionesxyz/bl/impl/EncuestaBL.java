2c1045fef4f2238256737f0f19d5699822c34e28
richypati@gmail.com <richypati@gmail.com@4ee7b030-5375-8fe9-563a-4aab63be8709>
2014-07-21 02:38:17 +0000

    Se implementa parte de la lógica del negocio

--- branches/rtf2VA/IW_PQRS_UdeA/src/main/co/com/inversionesxyz/bl/impl/EncuestaBL.java
+++ branches/rtf2VA/IW_PQRS_UdeA/src/main/co/com/inversionesxyz/bl/impl/EncuestaBL.java
@@ -1,28 +1,39 @@
 package co.com.inversionesxyz.bl.impl;
 
+import java.util.Date;
+
 import co.com.inversionesxyz.bl.IEncuestaBL;
 import co.com.inversionesxyz.dao.IEncuestaDAO;
+import co.com.inversionesxyz.dto.Encuesta;
 import co.com.inversionesxyz.exception.BasicDBOperationException;
 
-public class EncuestaBL implements IEncuestaBL{
-	
+public class EncuestaBL implements IEncuestaBL {
+
 	IEncuestaDAO encuestaDAO;
 
 	@Override
-	public void responderEncuesta(String idSolicitud,
-			String respuestaPreguntaUno, String respuestaPreguntaDos) throws BasicDBOperationException {
+	public void responderEncuesta(int idSolicitud, String respuestaPreguntaA,
+			String respuestaPreguntaB) throws BasicDBOperationException {
 		if ("".equals(idSolicitud)) {
 			throw new BasicDBOperationException(
 					"El id de la solicitud no puede ser vacía.");
 		}
-		if ("".equals(respuestaPreguntaUno)) {
-			throw new BasicDBOperationException("La respuesta 1 no puede ser vacía.");
+		if ("".equals(respuestaPreguntaA)) {
+			throw new BasicDBOperationException(
+					"La respuesta A no puede ser vacía.");
 		}
-		if ("".equals(respuestaPreguntaDos)) {
-			throw new BasicDBOperationException("La respuesta 2 no puede ser vacía.");
+		if ("".equals(respuestaPreguntaB)) {
+			throw new BasicDBOperationException(
+					"La respuesta B no puede ser vacía.");
 		}
-		
-		//encuestaDAO.saveRespuestas
+
+		Encuesta encuesta = new Encuesta();
+		encuesta.setSolicitudId(idSolicitud);
+		encuesta.setRespuestaPreguntaA(respuestaPreguntaA);
+		encuesta.setRespuestaPreguntaB(respuestaPreguntaB);
+		encuesta.setFechaEnvio(new Date());
+
+		encuestaDAO.insertar(encuesta);
 	}
 
 }
