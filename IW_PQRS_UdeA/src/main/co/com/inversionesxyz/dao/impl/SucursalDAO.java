78299975f0d54293f55a2153895594d87ac62789
jenniferperezb <jenniferperezb@gmail.com>
2014-07-23 00:37:59 +0000

    - Se actualiza DAO de sucursal

--- IW_PQRS_UdeA/src/main/co/com/inversionesxyz/dao/impl/SucursalDAO.java
+++ IW_PQRS_UdeA/src/main/co/com/inversionesxyz/dao/impl/SucursalDAO.java
@@ -1,84 +1,33 @@
-//package co.com.inversionesxyz.dao.impl;
-//
-//import org.apache.commons.logging.Log;
-//import org.apache.commons.logging.LogFactory;
-//import org.hibernate.SessionFactory;
-//
-//import co.com.inversionesxyz.dao.ISucursalDAO;
-//import co.com.inversionesxyz.dto.Sucursal;
-//
-///**
-// * Home object for domain model class Sucursal.
-// * @see .Sucursal
-// * @author Hibernate Tools
-// */
-//public class SucursalDAO extends AbstractDAO implements ISucursalDAO{
-//
-//	private static final Log log = LogFactory.getLog(SucursalDAO.class);
-//
-//	private final SessionFactory sessionFactory = getSessionFactory();
-//
-//	public void persist(Sucursal transientInstance) {
-//		log.debug("persisting Sucursal instance");
-//		try {
-//			sessionFactory.getCurrentSession().persist(transientInstance);
-//			log.debug("persist successful");
-//		} catch (RuntimeException re) {
-//			log.error("persist failed", re);
-//			throw re;
-//		}
-//	}
-//
-//	public void attachDirty(Sucursal instance) {
-//		log.debug("attaching dirty Sucursal instance");
-//		try {
-//			sessionFactory.getCurrentSession().saveOrUpdate(instance);
-//			log.debug("attach successful");
-//		} catch (RuntimeException re) {
-//			log.error("attach failed", re);
-//			throw re;
-//		}
-//	}
-//
-//	public void delete(Sucursal persistentInstance) {
-//		log.debug("deleting Sucursal instance");
-//		try {
-//			sessionFactory.getCurrentSession().delete(persistentInstance);
-//			log.debug("delete successful");
-//		} catch (RuntimeException re) {
-//			log.error("delete failed", re);
-//			throw re;
-//		}
-//	}
-//
-//	public Sucursal merge(Sucursal detachedInstance) {
-//		log.debug("merging Sucursal instance");
-//		try {
-//			Sucursal result = (Sucursal) sessionFactory.getCurrentSession()
-//					.merge(detachedInstance);
-//			log.debug("merge successful");
-//			return result;
-//		} catch (RuntimeException re) {
-//			log.error("merge failed", re);
-//			throw re;
-//		}
-//	}
-//
-//	public Sucursal findById(java.lang.String id) {
-//		log.debug("getting Sucursal instance with id: " + id);
-//		try {
-//			Sucursal instance = (Sucursal) sessionFactory.getCurrentSession()
-//					.get("Sucursal", id);
-//			if (instance == null) {
-//				log.debug("get successful, no instance found");
-//			} else {
-//				log.debug("get successful, instance found");
-//			}
-//			return instance;
-//		} catch (RuntimeException re) {
-//			log.error("get failed", re);
-//			throw re;
-//		}
-//	}
-//
-//}
+package co.com.inversionesxyz.dao.impl;
+
+import java.text.MessageFormat;
+
+import co.com.inversionesxyz.dao.ISucursalDAO;
+import co.com.inversionesxyz.dto.Sucursal;
+import co.com.inversionesxyz.exception.BasicDBOperationException;
+
+/**
+ * Clase que define las operaciones a realizar en base de datos sobre una Solicitud
+ * @see .Sucursal
+ * @author Jennifer Perez
+ * @author Ricardo Patino
+ */
+public class SucursalDAO extends AbstractDAO<Sucursal> implements ISucursalDAO{
+
+	public SucursalDAO() {
+		super(Sucursal.class);
+	}
+
+	@Override
+	public Sucursal consultarPorCodigo(String codigo) {
+		try{
+			return getByField(codigo);
+		}catch(Exception e){
+			throw new BasicDBOperationException(MessageFormat.format(
+					"No fue posible consultar la solicitud por campo: {0}",
+					codigo), e.getCause());
+		}
+	}
+
+	
+}
