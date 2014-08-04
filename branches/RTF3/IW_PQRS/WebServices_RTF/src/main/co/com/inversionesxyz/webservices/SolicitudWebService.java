b2d4444117864288cfd93f4562aadee5b06c3db0
richypati@gmail.com <richypati@gmail.com@4ee7b030-5375-8fe9-563a-4aab63be8709>
2014-08-04 00:26:11 +0000

    Se añaden los servicios de Consulta dde solicitudes por sucursal y de responder solicitud

--- branches/RTF3/IW_PQRS/WebServices_RTF/src/main/co/com/inversionesxyz/webservices/SolicitudWebService.java
+++ branches/RTF3/IW_PQRS/WebServices_RTF/src/main/co/com/inversionesxyz/webservices/SolicitudWebService.java
@@ -1,5 +1,8 @@
 package co.com.inversionesxyz.webservices;
 
+import java.text.MessageFormat;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -13,6 +16,8 @@ import org.springframework.stereotype.Component;
 
 import co.com.inversionesxyz.bussinesslogic.ISolicitudService;
 import co.com.inversionesxyz.dto.Solicitud;
+import co.com.inversionesxyz.exception.BasicDBOperationException;
+import co.com.inversionesxyz.exception.EmailException;
 
 @Path("solicitud")
 @Component
@@ -37,6 +42,28 @@ public class SolicitudWebService {
 	
 	public void setSolicitudService(ISolicitudService solicitudService) {
 		this.solicitudService = solicitudService;
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/consultarPorSucursal/{codigoSucursal}")
+	public List<Solicitud> consultarPorSucursal(@PathParam("codigoSucursal") String codigoSucursal){
+		return solicitudService.consultarPorSucursal(codigoSucursal);
+	}
+	
+	@POST
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Path("/darRespuestaASolicitud/{id}/{respuesta}")
+	public String darRespuestaASolicitud(@PathParam("id")int id, @PathParam("respuesta") String respuesta){
+		Solicitud solicitud = solicitudService.consultarSolicitud(id);
+		try {
+			solicitudService.ResponderSolicitud(solicitud, respuesta);
+		} catch (EmailException e) {
+			throw new BasicDBOperationException(MessageFormat.format(
+					"No fue posible dar respuesta a la solicitud: {0}", id),
+					e.getCause());
+		}
+		return "Se ha dado respuesta a la solicitud";
 	}
 
 }
