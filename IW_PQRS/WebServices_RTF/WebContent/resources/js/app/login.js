1e00ad4b091b829741993c857bc1f59468a5ef31
jenniferperezb <jenniferperezb@gmail.com>
2014-08-28 02:34:33 +0000

    - Se agrega html de login

---
+++ IW_PQRS/WebServices_RTF/WebContent/resources/js/app/login.js
@@ -0,0 +1,10 @@
+	var validationApp = angular.module('loginApp', []);
+	validationApp.controller('loginController', function($scope) {
+		$scope.submitForm = function(isValid) {
+			if (isValid) {
+				alert('our form is amazing');
+			}
+
+		};
+
+	});
