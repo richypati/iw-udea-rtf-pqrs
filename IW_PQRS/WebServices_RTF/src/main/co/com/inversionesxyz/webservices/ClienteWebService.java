aa61010fa704c060776400afe21bd8d4375b8269
richypati <richypati@gmail.com>
2014-08-29 00:11:05 +0000

    Muchos cambios -.-

---
+++ IW_PQRS/WebServices_RTF/src/main/co/com/inversionesxyz/webservices/ClienteWebService.java
@@ -0,0 +1,42 @@
+package co.com.inversionesxyz.webservices;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("cliente")
+public class ClienteWebService {
+
+	String cliente[][] = { { "richypati@gmail.com", "12345", "admin" },
+			{ "jenniferperezb@gmail.com", "54321", "cliente" },
+			{ "6969", "6969", "analista" }, { "12345", "12345", "analista" } };
+
+	@GET
+	@Path("/validarCliente")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response validarCliente(
+			@QueryParam("emailCliente") String emailCliente,
+			@QueryParam("password") String password) {
+
+		if (!clienteValido(emailCliente, password)) {
+			return Response.ok("Nombre de usuario o contraseña incorrectos")
+					.build();
+		}
+
+		return Response.ok("ok").build();
+	}
+
+	private boolean clienteValido(String emailCliente, String password) {
+		for (int i = 0; i < cliente.length; i++) {
+			if (cliente[i][0].equalsIgnoreCase(emailCliente)
+					&& cliente[i][1].equals(password)) {
+				return true;
+			}
+
+		}
+		return false;
+	}
+}
